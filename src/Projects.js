9f5063f96071bdf8deb1bb34ce22f9f9eb5206b3
Ramagiri shiva shankara prasad <64155226+Shiva143-debug@users.noreply.github.com>
2026-03-05 12:45:18 +0530

    Update Projects.js

--- src/Projects.js
+++ src/Projects.js
@@ -43,7 +43,8 @@ const Projects = () => {
       description: 'A job search application where users can find and apply for job openings.',
       category: 'frontend',
       image: 'https://res.cloudinary.com/dxgbxchqm/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1718546328/jobby-app-jobs-success-lg-output-v0_vb2df6.png',
-      link: 'https://job-ap.vercel.app/'
+      // link: 'https://job-ap.vercel.app/'
+      link:'https://job-portal.shiva-tech.in'
     },
     {
       title: 'Instagram Clone',
